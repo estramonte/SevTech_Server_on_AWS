2cd81f2ae6e46e00a66343861407badfe10c3bbd
estramonte <89673824+estramonte@users.noreply.github.com>
2025-01-21 19:29:34 -0800

    add start and stop discord scripts

---
+++ server-scripts/start-discord-bot.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Start the Discord bot
+cd /home/ubuntu/SevTechServer/SevTechServer/SevTech_Server_on_AWS/discord-bot
+nohup node index.js &
---
+++ server-scripts/stop-discord-bot.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Stop the Discord bot
+pkill -f "node index.js"
